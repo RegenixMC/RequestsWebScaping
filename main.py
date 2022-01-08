f46be63bec0949c8f99d11705f4f3e87b5223135
Regenix <73912088+RegenixMC@users.noreply.github.com>
2022-01-07 21:04:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,44 +2,78 @@ from bs4 import BeautifulSoup
 from lxml import etree
 import requests
 import time
+import socket
+from pushbullet import Pushbullet
+import datetime
 
 # CONFIG START
+pushbulletAPI_KEY = 'o.S1dbtZulvxmAnLJeWcJlKOfwF2N2rXNd'
+
+
 microcenterItem = "https://www.microcenter.com/product/470832/Crystal_570X_RGB_ATX_Mid-Tower_Computer_Case_-_Black-Clear"
 delay = True
-printItemElement = False
+printItem = True
 # CONFIG END
+
+pb = Pushbullet(pushbulletAPI_KEY)
+
+count = 0
+printItemElement = False
+def isConnected():
+    try:
+        sock = socket.create_connection(("www.speedtest.net", 80))
+        if sock is not None:
+            sock.close
+        return True
+    except OSError:
+        pass
+    return False
 
 
 def inStockAlert():
     print('In Stock!!!!!!!!!!!!')
+    push = pb.push_note('Item in stock!', 'your item is now in stock on microcenter!')
 
 while True:
-    HEADERS = ({'User-Agent':
-                'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
-                (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',\
-                'Accept-Language': 'en-US, en;q=0.5'})
-    
-    webpage = requests.get(microcenterItem, headers=HEADERS)
-    soup = BeautifulSoup(webpage.content, "html.parser")
-    dom = etree.HTML(str(soup))
+    print('checking for wifi...')
+    if (isConnected() == True):
+        print('Wifi connected!')
+        HEADERS = ({'User-Agent':
+                    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 \
+                    (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',\
+                    'Accept-Language': 'en-US, en;q=0.5'})
 
 
-    try:
-        itemStatus = int(dom.xpath('/html/body/main/article/div[3]/div[1]/div[1]/div/div[2]/div[1]/p/span[2]')[0].text)
+        webpage = requests.get(microcenterItem, headers=HEADERS)
+        soup = BeautifulSoup(webpage.content, "html.parser")
+        dom = etree.HTML(str(soup))
 
-        if printItemElement:
-            print(itemStatus)
+        print('Opened webpage.')
+        
         try:
-            if (itemStatus != 0):
+            itemStatus = int(dom.xpath('/html/body/main/article/div[3]/div[1]/div[1]/div/div[2]/div[1]/p/span[2]')[0].text)
+            
+            if printItemElement:
+                print(itemStatus)
+            try:
+                if (itemStatus != 0):
+                    inStockAlert()
+                    break
+                else:
+                    if printItem:
+                        count += 1
+                        print('Out of Stock! ' + str(count))
+                    if delay:
+                        time.sleep(5)
+                    
+            except:
                 inStockAlert()
                 break
-            else:
-                if printItemElement:
-                    print('Out of Stock!')
-                if delay:
-                    time.sleep(5)
+                
+                
         except:
             inStockAlert()
-            break
-    except:
-        inStockAlert()
+
+    else:
+        print('No wifi...')
+        time.sleep(3)
